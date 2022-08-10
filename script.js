91e5357aa8c6c97287fcceb4afc70d1abc2ab24d
JesseC <JQCruz98@gmail.com>
2022-08-09 19:00:11 -0700

    Add some style to index page, game logic almost complete.

--- script.js
+++ script.js
@@ -39,20 +39,46 @@ let loseCounter = 0;
 
 function getPlayerChoice(objButton){
     let playerChoice = (objButton.value);
+    console.log(playerChoice);
+    return playerChoice;
+}
+
+function playGame(playerChoice)
+{
     let computerChoice = (getComputerChoice());
+    
     if(playerWin[playerChoice] === computerChoice){
-        alert("you win!");
+        console.log("you won the round")
         winCounter++;
         console.log(`Wins: ${winCounter} Losses: ${loseCounter}`);
     }
     else if(playerLost[playerChoice] === computerChoice){
-        alert("You lost!");
+        console.log("you lost the round")
         loseCounter++;
         console.log(`Wins: ${winCounter} Losses: ${loseCounter}`);
     }
     else{
-        alert("draw!")
+        console.log("draw")
+    }
+
+}
+
+function gameOver(){
+    if (winCounter == 5){
+        alert("You Win!")
+        console.log("you win")
+    }
+    else if(loseCounter == 5){
+        alert("You lost :(")
     }
 }
 
-console.log(getComputerChoice())
+
+const choices = document.getElementsByClassName("choice");
+console.log(choices);
+for(let i = 0; i <= 2; i++){
+    choices[i].addEventListener('click', function() {playGame(getPlayerChoice(this))});
+    choices[i].addEventListener('click', function(){gameOver()});
+}
+
+
